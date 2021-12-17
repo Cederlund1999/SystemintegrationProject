b5ab4278cbb80953ac5e3067f616247a60443de1
Gustav Cederlund <70195738+Cederlund1999@users.noreply.github.com>
2021-12-17 16:41:16 +0100

    Start to BasketService

---
+++ FreakyFashionServices.BasketService/Controllers/BasketsController.cs
@@ -0,0 +1,42 @@
+﻿using FreakyFashionServices.BasketService.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace FreakyFashionServices.BasketService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        public BasketsController(IDistributedCache cache)
+        {
+            Cache = cache;
+        }
+
+        public IDistributedCache Cache { get; }
+
+
+        [HttpPut("{customerId}")]
+        public IActionResult CreateBasket(BasketDto basketDto)
+        {
+            var serializedBasket = JsonSerializer.Serialize(basketDto);
+
+            Cache.SetString(basketDto.CustomerId, serializedBasket);
+
+            return NoContent();
+        }
+        [HttpGet("{customerId}")]
+        public ActionResult<BasketDto> GetBasket(string customerId)
+        {
+            var serializedBasket = Cache.GetString(customerId);
+            if(serializedBasket == null)
+            {
+                return NotFound();
+            }
+            var basketDto = JsonSerializer.Deserialize<BasketDto>(serializedBasket);
+            return Ok(basketDto);
+        }
+    }
+}
---
+++ FreakyFashionServices.BasketService/Models/DTO/BasketDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreakyFashionServices.BasketService.Models.DTO
+{
+    public class BasketDto
+    {
+        
+        public string CustomerId { get; set; }
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        
+    }
+}
